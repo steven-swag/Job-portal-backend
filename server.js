2aba53ff72d96299955f81d270d0074a09914703
Steven <stevensharon875@gmail.com>
2026-06-13 02:00:34 +0530

    Fix CORS preflight

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
-      'https://job-portal-frontend-xwrx.vercel.app',
+      'https://job-portal-frontend-iota-five.vercel.app',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
