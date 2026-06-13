3bd59c80cf2e4a3db647b8d93d35a49f58de7086
Steven <stevensharon875@gmail.com>
2026-06-13 10:07:11 +0530

    Debug nodemailer

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,24 +2,36 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (email, subject, text) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
   });
 
   console.log('Verifying transporter');
+
+  await transporter.verify();
+
+  console.log('Transporter verified');
+
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: email,
     subject,
     text,
   };
-  console.log('Transporter verified');
 
-  await transporter.sendMail(mailOptions);
-  console.log('Mail sent');
+  console.log('Before sendMail');
+
+  const info = await transporter.sendMail(mailOptions);
+
+  console.log('Mail sent:', info.messageId);
 };
 
 module.exports = sendEmail;
